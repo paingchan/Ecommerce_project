83de6f8388e8b45d7fd75312d1f4a6dba6180c97
Paing Chan <aung.piang.chan32@gmail.com>
2022-07-17 22:42:59 +0630

    commit changes

---
+++ assets/js/jquery.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+    
+    add_cart();
+    
+})
+
+
+
+function add_cart()
+{
+    $(document).on('click' , '#id' , function() {
+       // var qty = $('#qty').val();
+       // var pid = $('#id').val();
+        console.log('working');
+       // $.ajax()
+    } )
+}
+
